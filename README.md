65bfe5179f8a15ea7d3e9c9be53703b91c0922a6
patrice gargiolo <pat-rice@ntymail.com>
2018-01-28 13:39:45 +0100

    retrieved all the information needed to do basic actions

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
 
 *Slack bot making daily stand up easier*
 
+##Installation
+
+
+##Run it
+
+You must set 2 environnment variables:
+- `SLACK_TOKEN` : Auth token that you should retrieve from slack interface
+- `TARGET_CHANNEL` : Name of the channel that will be used by botsketeer. All members of this channel will be subscribed to the service.
+
--- main.js
+++ main.js
@@ -2,25 +2,15 @@ const {WebClient, RtmClient, CLIENT_EVENTS, RTM_EVENTS, RTM_MESSAGE_SUBTYPES} = 
 
 // An access token (from your Slack app or custom integration - usually xoxb)
 const token = process.env.SLACK_TOKEN;
-
+const targetChannelName = process.env.TARGET_CHANNEL;
 ///////////////
 
 
 const web = new WebClient(token);
-let testBotChannel;
-
-// See: https://api.slack.com/methods/chat.postMessage
-web.channels.list()
-  .then((res) => {
-    testBotChannel = res.channels.find((channel) => {
-      return channel.name === 'test-bot';
-    });
-    console.log('gotChannel', JSON.stringify(testBotChannel));
-  })
-  .catch(console.error);
-
-////////////
-
+let targetChannel;
+let membersChannel = [];
+let membersDirectory = [];
+let directMessagesChannels = {};
 
 // Cache of data
 const appData = {};
@@ -32,24 +22,27 @@ const rtm = new RtmClient(token, {
   useRtmConnect: true,
 });
 
-// The client will emit an RTM.AUTHENTICATED event on when the connection data is avaiable
-// (before the connection is open)
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (connectData) => {
-  // Cache the data necessary for this app in memory
   appData.botData = connectData.self;
   console.log(`Logged in as ${JSON.stringify(appData)} of team ${connectData.team.id}`);
 });
 
+//Subscribing all channel memebers to presence events
+rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
+  rtm.subscribePresence(targetChannel.members);
+});
+
 let conversations = {};
 
+rtm.on(RTM_EVENTS.PRESENCE_CHANGE, (userPresence) => {
+  let currentUser = membersChannel.find((u) => u.id === userPresence.user);
+  if (currentUser && userPresence.presence === 'active' && !conversations[currentUser.id]) {
+    web.chat.postMessage(directMessagesChannels[currentUser.id], `Hello ${currentUser.profile.first_name} ! It's time for our standup meeting! What did you do yesterday ?`);
+  }
+});
+
 rtm.on(RTM_EVENTS.MESSAGE, (rawMessage) => {
-  console.log(rawMessage);
-
-  if (rawMessage.type === 'message' && rawMessage.subtype != RTM_MESSAGE_SUBTYPES.BOT_MESSAGE) {
-
-    if (rawMessage.text === 'report') {
-
-    }
+  if (rawMessage.type === RTM_EVENTS.MESSAGE && rawMessage.subtype != RTM_MESSAGE_SUBTYPES.BOT_MESSAGE) {
 
     if (conversations[rawMessage.user]) {
       conversations[rawMessage.user].push(rawMessage.text);
@@ -57,30 +50,50 @@ rtm.on(RTM_EVENTS.MESSAGE, (rawMessage) => {
       conversations[rawMessage.user] = [rawMessage.text];
     }
 
-
-    console.log('posting back message');
-    web.chat.postMessage(rawMessage.channel, 'AHAHAH ' + rawMessage.text);
     if (conversations[rawMessage.user].length === 3) {
       let messageText = 'Here\'s and update!';
       let messageOptions = {
-        "attachments": []
+        // as_user    : true,
+        attachments: []
       };
 
       conversations[rawMessage.user].forEach((line, index) => {
         messageOptions.attachments.push({
-          "fallback": "Required plain-text summary of the attachment.",
-          "color"   : ["#36a64f", '#4996ff', '#fff000'][index],
-          "pretext" : "*Optional text that appears above the attachment block*",
-          "text"    : line,
+          fallback: 'Required plain-text summary of the attachment.',
+          color   : ['#36a64f', '#4996ff', '#fff000'][index],
+          pretext : '*Optional text that appears above the attachment block*',
+          text    : line,
         });
       });
 
       //todo Should post as user
-      web.chat.postMessage(testBotChannel.id, messageText, messageOptions);
+      web.chat.postMessage(targetChannel.id, messageText, messageOptions);
       conversations[rawMessage.user] = [];
     }
   }
 });
 
-// Start the connecting process
-rtm.start();
+// Fetch all users, find the target channel, and members of this channel
+web.users.list()
+  .then((res) => {
+    //Get users linked to the app instead
+    membersDirectory = res.members.filter((u) => !u.deleted && !u.is_bot && u.name !== 'slackbot');
+    return web.channels.list();
+  })
+  .then((res) => {
+    targetChannel = res.channels.find((channel) => {
+      return channel.name === targetChannelName;
+    });
+    membersChannel = membersDirectory.filter(u => targetChannel.members.indexOf(u.id) !== -1);
+
+    return web.im.list();
+  })
+  .then((res) => {
+    //Mapping user ids to direct message channels ids
+    targetChannel.members.forEach((userId) => {
+      directMessagesChannels[userId] = res.ims.find((im) => targetChannel.members.indexOf(im.user) !== -1).id;
+    });
+
+    rtm.start();
+  })
+  .catch(console.error);
